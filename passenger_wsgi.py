e99ad9849c88522c427113b3c467e457bf4c5b90
Bala Abhilash Reddy Yeruva <byeruv316@cable.comcast.com>
2026-07-26 22:29:21 -0400

    Fix Passenger startup path

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,3 +1,10 @@
+import os
+import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent
+sys.path.insert(0, str(ROOT_DIR))
+
 from app import create_app
 
 application = create_app()
